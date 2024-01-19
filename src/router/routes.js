96b24ff7692c875f7f459c366e39653cdab15d62
Pierre DALLARA <pierre1.dallara@epitech.eu>
2024-01-19 12:56:47 +0100

    [DEL] MqttPage not used anymore / it was template for Dashboard use mqtt

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,27 +1,32 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '/', component: () => import('src/pages/LoginPage.vue') },
-      { path: '/register', component: () => import('src/pages/RegisterPage.vue') },
-      { path: '/dashboard', component: () => import('src/pages/DashboardPage.vue') },
-      { path: '/settings', component: () => import('src/pages/SettingsPage.vue') },
-      { path: '/mqtt', component: () => import('src/pages/MqttPage.vue') },
-      { path: '/admin', component: () => import('src/pages/AdminPage.vue') },
-      { path: '/sensor', component: () => import('src/pages/SensorPage.vue') },
-
-
-    ]
+      { path: "/", component: () => import("src/pages/LoginPage.vue") },
+      {
+        path: "/register",
+        component: () => import("src/pages/RegisterPage.vue"),
+      },
+      {
+        path: "/dashboard",
+        component: () => import("src/pages/DashboardPage.vue"),
+      },
+      {
+        path: "/settings",
+        component: () => import("src/pages/SettingsPage.vue"),
+      },
+      { path: "/admin", component: () => import("src/pages/AdminPage.vue") },
+      { path: "/sensor", component: () => import("src/pages/SensorPage.vue") },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
